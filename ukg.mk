52e1dbfc5e9919b0a29f553f77c19ad243935102
David Hou <david.hou314@gmail.com>
2013-05-18 20:51:36 -0700

    This seems to work

--- ukg.mk
+++ ukg.mk
@@ -2,16 +2,16 @@
 $(call inherit-product, device/samsung/jfltetmo/full_jfltetmo.mk)
 
 # Inherit common product files.
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/ukg/config/common.mk)
 
 # Inherit GSM common stuff
-$(call inherit-product, vendor/aosp/config/gsm.mk)
+$(call inherit-product, vendor/ukg/config/gsm.mk)
 
 # Mako Overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/aosp/overlay/toro
+PRODUCT_PACKAGE_OVERLAYS += vendor/ukg/overlay/toro
 
 # Setup device specific product configuration.
-PRODUCT_NAME := xylon_jfltetmo
+PRODUCT_NAME := ukg_jfltetmo
 PRODUCT_BRAND := Samsung
 PRODUCT_DEVICE := jfltetmo
 PRODUCT_MODEL := SGS4
@@ -21,6 +21,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltetmo TARGET_DEVICE=jfltetmo BUI
 
 # Copy mako specific prebuilt files
 PRODUCT_COPY_FILES +=  \
-    vendor/aosp/prebuilt/hybrid_xhdpi.conf:system/etc/beerbong/properties.conf \
-    vendor/aosp/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
-    vendor/aosp/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
+    vendor/ukg/prebuilt/hybrid_xhdpi.conf:system/etc/beerbong/properties.conf \
+    vendor/ukg/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
+    vendor/ukg/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
